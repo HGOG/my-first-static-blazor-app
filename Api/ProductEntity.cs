8bce6d09b357b3948e5da1c3879219c039cbee82
Keith Harman <keithaharman@outlook.com>
2021-03-22 19:42:23 +0000

    Changes to api area of project

---
+++ Api/ProductEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage.Table;
+
+
+namespace BlazorApp.Api
+{
+   public class ProductEntity : TableEntity
+    {
+        public ProductEntity()
+        {
+
+        }
+        public ProductEntity(string id)
+        {
+            RowKey = id;
+            PartitionKey = id;
+        }
+
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public DateTime WalkDate { get; set; }
+        
+
+    }
+}
